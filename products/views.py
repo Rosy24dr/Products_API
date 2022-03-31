dd26a86ea57af67b9189992e91d4bab1955f0d6f
rossebell <Rosy24dr@outlook.com>
2022-03-31 16:06:29 -0400

    created the products  urls & created the views function & the products projet urls.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from xmlrpc.client import ResponseError
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def products_lists(request):
+    return Response('God is with me!')
