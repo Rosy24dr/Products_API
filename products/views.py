2cc84a529c18c8cc5def072a91ec34615e18f58e
rossebell <Rosy24dr@outlook.com>
2022-03-31 17:25:48 -0400

    Registered our model with the admin file. Added POST functionality.

--- products/views.py
+++ products/views.py
@@ -1,12 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_lists(request):
-    products = Product.objects.all()
-    serializers = ProductSerializer(products, many = True)
 
-    return Response(serializers.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializers = ProductSerializer(products, many = True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializers = ProductSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
