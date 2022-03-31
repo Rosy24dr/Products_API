684297fe554709aef6ab910b32e5bf6776074b02
rossebell <Rosy24dr@outlook.com>
2022-03-31 18:26:58 -0400

    Created the PUT functionality to update a product

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-from products import serializers
+
 
 
 @api_view(['GET', 'POST'])
@@ -20,12 +21,16 @@ def products_lists(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def products_detail(request,pk):
-    try: 
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        product = get_object_or_404(Product,pk=pk)
+        if request.method == 'GET':
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        elif request.method == 'PUT':
+            serializer = ProductSerializer(product, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+    
     
