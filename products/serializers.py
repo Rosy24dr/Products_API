2bb4fe1f6e576d2f7f7c19b95558a04472d6d5ae
rossebell <Rosy24dr@outlook.com>
2022-03-31 16:22:18 -0400

    Created the products list function and postman GET request

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,3 @@
-from dataclasses import field
-from socket import CAN_RAW
 from rest_framework import  serializers
 from .models import Product
 
--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
-from xmlrpc.client import ResponseError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
 
 @api_view(['GET'])
 def products_lists(request):
-    return Response('God is with me!')
+    products = Product.objects.all()
+    serializers = ProductSerializer(products, many = True)
+
+    return Response(serializers.data)
