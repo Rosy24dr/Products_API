abc0fffb2e12b7abc5ab38f66913e02bc34f9cf4
rossebell <Rosy24dr@outlook.com>
2022-04-01 16:03:51 -0400

    Created the image property

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.CharField(max_length=200)
